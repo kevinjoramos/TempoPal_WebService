bd80e9dcfd941826edd8d58056bbbeb5aaa0f2fe
Kevin Ramos <kevin.jo.ramos@gmail.com>
2023-10-06 15:01:00 -0600

    migrating project to spring boot instead of ktor.

--- src/main/kotlin/tempopal/schema/TagSchema.kt
+++
@@ -1,8 +0,0 @@
-package tempopal.schema
-
-data class Tag(
-    val userId: Int,
-    val tagId: Int,
-    val tagName: String,
-    val childrenTrackedItems: List<Int>
-)
--- src/main/kotlin/tempopal/schema/UserSchema.kt
+++
@@ -1,7 +0,0 @@
-package tempopal.schema
-
-data class UserSchema(
-    val userId: Int,
-    val userName: String,
-    val userEmail: String
-)
