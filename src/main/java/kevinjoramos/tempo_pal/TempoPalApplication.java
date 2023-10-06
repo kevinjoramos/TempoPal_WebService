c23d1ce07f9d89d6a8ad24fb145603e7f53fbade
Kevin Ramos <kevin.jo.ramos@gmail.com>
2023-10-06 15:18:45 -0600

    Created spring boot project.

---
+++ src/main/java/kevinjoramos/tempo_pal/TempoPalApplication.java
@@ -0,0 +1,13 @@
+package kevinjoramos.tempo_pal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TempoPalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TempoPalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kevinjoramos/tempo_pal/TempoPalApplicationTests.java
@@ -0,0 +1,13 @@
+package kevinjoramos.tempo_pal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TempoPalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
