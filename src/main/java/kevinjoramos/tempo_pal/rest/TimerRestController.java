ca2b92c9950e0f246cc9c92d2fef550e2a24d034
Kevin Ramos <kevin.jo.ramos@gmail.com>
2023-10-12 05:46:37 -0600

    Created stubs files for sql scripts.
    These will remain blank on the repo because I don't want these public.

--- src/main/java/kevinjoramos/tempo_pal/rest/TimerRestController.java
+++ src/main/java/kevinjoramos/tempo_pal/rest/TimerRestController.java
@@ -1,4 +1,15 @@
 package kevinjoramos.tempo_pal.rest;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
 public class TimerRestController {
+
+    @GetMapping("/timers")
+    public String greeting() {
+        return "Hello World!";
+    }
 }
